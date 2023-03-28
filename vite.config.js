1409306a2083de5938b5adb9c0886fe4694bc7f1
Gabriel Paredes <gabriel@axented.com>
2023-03-28 17:45:28 -0600

    Update vite.config.js settings to include outDir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ const path = require('path')
 export default {
   base: '/chilakart',
   root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: false
+  },
   resolve: {
     alias: {
         '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
